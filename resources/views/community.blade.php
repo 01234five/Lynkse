444db505a44c4c905105efe9066bccced73ccea3
01234five <83076267+01234five@users.noreply.github.com>
2021-05-28 21:16:40 -0700

    update 5
    fixed chat timeago.js Problem that was being called before DOM object was created.

--- resources/views/community.blade.php
+++ resources/views/community.blade.php
@@ -1759,7 +1759,7 @@ if(online==false){
 </li>
 
         `);
-    }).then(function () {console.log("inserted li");lastConversations(id);}).then(function () {console.log("done");timeago.render(document.querySelectorAll('.need_to_be_rendered'));})
+    })
     .catch(function (error) {
         // handle error
         console.log(error);
@@ -1805,14 +1805,14 @@ if(online==false){
 </li>
 
         `);
-    }).then(function () {console.log("inserted li");lastConversations(id);}).then(function () {console.log("done");timeago.render(document.querySelectorAll('.need_to_be_rendered'));})
+    })
     .catch(function (error) {
         // handle error
         console.log(error);
     }); 
   }
     
-
+  lastConversations(id)
     });
     
 
